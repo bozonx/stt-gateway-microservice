99d3ee33d29d4b9487244a2961a01ec6f0b2e99d
Ivan <ipkozyrin@gmail.com>
2025-11-04 16:00:20 -0300

    fix

--- src/modules/index/index.module.ts
+++ src/modules/index/index.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
-import { IndexController } from './index.controller';
 
 @Module({
-  controllers: [IndexController],
+  controllers: [],
 })
 export class IndexModule {}
